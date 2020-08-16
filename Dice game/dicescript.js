d1cc411c664a4fbcfd886c1d69486ce4dc315e2f
Ramya <ramyashrihebbar501@gmail.com>
2020-08-16 10:13:23 +0530

    init

--- Dice game/dicescript.js
+++ Dice game/dicescript.js
@@ -1,25 +1,33 @@
-const randNumber1 = Math.floor(Math.random()*6)+1;
+const randNumber1 = Math.floor(Math.random() * 6) + 1;
 console.log(randNumber1);
 
-const diceNum = `dice$(randNumber1).png`
-console.log(diceNum);
+const diceNumber = `dice${randNumber1}.png`;
+console.log(diceNumber);
 
-const diceImg = `images/$(diceNum)`;
-console.log(diceImg);
+const diceImage = `images/${diceNumber}`;
+console.log(diceImage);
 
-const image = document.querySelector('.img1');
-image.setAttribute("src" ,diceImg);
+const image = document.querySelector(".img1");
 
-//dice 2
+image.setAttribute("src", diceImage);
 
-const randNumber2 = Math.floor(Math.random()*6)+1;
-console.log(randNumber2);
+const randNumber2 = Math.floor(Math.random() * 6) + 1;
+console.log(randNumber1);
 
-const diceNum2 = `dice$(randNumber2).png`
-console.log(diceNum2);
+const diceNumber2 = `dice${randNumber2}.png`;
+console.log(diceNumber);
 
-const diceImg2= `images/$(diceNum2)`;
-console.log(diceImg2);
+const diceImage2 = `images/${diceNumber2}`;
+console.log(diceImage);
 
-const image2= document.querySelector('.img2');
-image2.setAttribute("src" ,diceImg2);
+const image2 = document.querySelector(".img2");
+
+image2.setAttribute("src", diceImage2);
+
+if (randNumber1 > randNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 won!!❤❤";
+} else if (randNumber2 > randNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 won!!❤❤";
+} else {
+  document.querySelector("h1").innerHTML = "Tie";
+}
